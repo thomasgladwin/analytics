e8b31f592054663ebe0e01e074350999c94a29b2
Thomas Edward Gladwin <thomas.gladwin@gmail.com>
2025-06-30 22:15:18 +0100

    Add files via upload

---
+++ VisitLogs.php
@@ -0,0 +1,47 @@
+ <?php
+try {
+	require("open_conn.php");
+	
+	$pw = $_GET["password"];
+	if ($pw != $password_get) {
+		return;
+	}
+	
+	try {	
+		$sql = "use ".$db_name;
+		$result = $conn->query($sql);
+	} catch(PDOException $e) {
+	}
+	
+	try {
+		$visit_id = $_GET["vid"];
+	} catch(Exception $e) {
+		$visit_id = "0";
+	}
+	try {
+		$current = strtok($_GET["current"], '?');
+	} catch(Exception $e) {
+		$current = "0";
+	}
+	try {
+		$target = strtok($_GET["target"], '?');
+	} catch(Exception $e) {
+		$target = "0";
+	}
+	$this_datetime = date('Y-m-d H:i:s');
+
+	try {	
+		$sql = "INSERT INTO VisitLogs (visit_id, current, target, log_time) VALUES (?, ?, ?, ?)";
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("ssss", $visit_id, $current, $target, $this_datetime);
+		$stmt->execute();
+	} catch(PDOException $e) {
+		echo $e;
+	}
+	$conn->close();
+
+} catch (Exception $e) {
+	echo $e;
+}
+
+?> 
---
+++ logging_passwords.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "mysql.xxx.com";
+$username = "xxx";
+$password = "xxx";
+$db_name = "xxx";
+
+// Other passwords
+$password_get = "xxx"; # For VisitLogs.php
+$reset_page_password = 'xxx'; # For database reset page
+?> 
